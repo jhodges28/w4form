708832b4e8564b71cd37b53359613aa1b22e7113
Caleb Sylvest <caleb.sylvest@gmail.com>
2014-02-21 22:40:24 -0600

    add a bunch of stuff. don't even know what. don't care.

--- _/js/functions.js
+++ _/js/functions.js
@@ -16,6 +16,54 @@
 $(document).ready(function (){
 
 	// your functions go here
+
+	var $submit = $(".submit input");
+	var $required = $(".required");
+	function containsBlanks(){
+		var blanks = $required.map(function(){ return $(this).val() == "";});
+		return $.inArray(true, blanks) != -1;
+	}
+
+	function isValidEmail(email){
+		return email.indexOf("@") != -1;
+	}
+
+	function requiredFilledIn(){
+		if(containsBlanks() || !isValidEmail($("#email").val())) 
+			$submit.attr("disabled","disabled");
+		else 
+			$submit.removeAttr("disabled");
+	}
+
+	$("#form span").hide();
+	$("input,textarea").focus(function(){
+		$(this).next().fadeIn("slow");
+	}).blur(function(){
+		$(this).next().fadeOut("slow");
+	}).keyup(function(){
+		//Check all required fields.
+		requiredFilledIn();
+	});
+
+	$("#email").keyup(function(){
+		//Check for a valid email.
+		if(isValidEmail($(this).val()))
+		 $(this).next().removeClass("error").addClass("valid");
+		else 
+		 $(this).next().removeClass("valid").addClass("error");
+	});
+
+	requiredFilledIn();
+
+	/*var firstName = "Caleb";
+	var middleName = "S";
+	var lastName = "Sylvest";
+	var streetAddress = "312 S. Kentucky Street";
+	var city = "McKinney":
+	var state = "TX";
+	var zipCode = "75069";*/
+
+
   
 });
 
